0765213716fda7555c13a796ac7b24c174516e4d
E-Justin <67754579+E-Justin@users.noreply.github.com>
2021-12-08 13:29:18 -0600

    Update billsProject.py
    still need to do some more testing
    userName only accepts one name, not first and last
    then have it write data to a file.

--- billsProject.py
+++ billsProject.py
@@ -12,26 +12,36 @@ user1Amounts= [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.] # array to hold user1's b
 user1Bills = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '] # array to hold string labeling what bill was paid  max of 10 bills
 user2Amounts= [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.] # array to hold user1's bill amounts   max of 10 bills
 user2Bills = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '] # array to hold string labeling what bill was paid  max of 10 bills
+user1total = 0
+user2total = 0
+
+def getTotals(userAmounts):
+    i = 0
+    total = 0
+    while userAmounts[i] != 0. and i < 10:
+        total += userAmounts[i]
+        i += 1
+    return total
 
 #method verifies that input is a string and returns the value if so
-def verifyAndGetName():
-    userInput = str(input(" What is person 1's name? "))
-    while userInput.isalpha() == False: # if userInput is a string
+def verifyAndGetName(userNumber):
+    userInput = str(input(" What is person %d's name? " % userNumber))
+    while userInput.isalpha() == False: # if userInput is not a string
         print ("***** ERROR ***** ")
-        print (" Please enter a name (with letters) ")
-        userInput = str(input(" What is person 1's name? "))
-        if userInput.isalpha == True: 
+        print (" Please enter a name ")
+        userInput = str(input(" What is person %d's name? " % userNumber))
+        if userInput.isalpha == True: # if it is a string
             break
     return userInput
 
-
+# this method verifies and gets the bill name
 def verifyAndGetBillName(userName):
     userInput = str(input(" What is the name of the bill you want to add for " + userName + "? "))
     while userInput.isalpha() == False: # if userInput is a string
         print ("***** ERROR ***** ")
-        print (" Please enter a bill name (with letters) ")
+        print (" Please enter a bill name ")
         userInput = str(input(" What is the name of the bill you want to add for " + userName + "?  "))
-        if userInput.isalpha == True: 
+        if userInput.isalpha == True: # if userInput is a number
             break
     return userInput
 
@@ -40,9 +50,10 @@ print (now) # displays the current date/ time
 print (" This program will have each user input their bills and amounts ")
 print (" This program is designed to have each user end up paying the same amount (50% - 50%) ")
 
-user1Name = verifyAndGetName()
-# user2Name = str(input(" What is person 2's name? "))
+user1Name = verifyAndGetName(1)
+user2Name = verifyAndGetName(2)
 
+# verifies that character entered is expected y/n for the first time
 def roundOneYesNoErrorCheck(userInput, userName):
     while userInput != 'y' and userInput != 'Y' and userInput != 'n' and userInput != 'N': # while input is a valid character
         print ("********** ERROR **********") # display error message if input is not a valid character
@@ -50,6 +61,7 @@ def roundOneYesNoErrorCheck(userInput, userName):
         userInput = str(input(" Would you like to enter bills for " + userName + "? y/n "))
     return userInput
 
+# verifies that character entered is expected y/n for the second and so on times
 def againYesNoErrorCheck(userInput, userName):
     while userInput != 'y' and userInput != 'Y' and userInput != 'n' and userInput != 'N': # while input is a valid character
             print ("********** ERROR **********") # display error message if input is not a valid character
@@ -63,9 +75,9 @@ def getBills(userName, userAmount, userBills):
     count = 0
     userContinue = str(input(" Would you like to enter bills for " + userName + "? y/n "))
 
-    userContinue = roundOneYesNoErrorCheck(userContinue, user1Name) # check to see if input was a valid character
+    userContinue = roundOneYesNoErrorCheck(userContinue, user1Name) # check to see if input was a valid , if so, store it in userContinue
 
-    while userContinue == 'y' or userContinue == 'Y':
+    while userContinue == 'y' or userContinue == 'Y': # if the user wishes to continue
         if count == 0: # first prompt for bill input
             userBills[index] = verifyAndGetBillName(userName)
             userAmount[index] = float(input(" What is the amount that " + userName + " paid for " + userBills[index] + "? "))
@@ -88,12 +100,30 @@ def getBills(userName, userAmount, userBills):
     return userAmount, userBills
 
 getBills(user1Name, user1Amounts, user1Bills) # method call for user1 bills and amounts
+getBills(user2Name, user2Amounts, user2Bills) # method call for user1 bills and amounts
 
+user1total = getTotals(user1Amounts)
+user2total = getTotals(user2Amounts)
+print("\n")
+print (" %s's total amount in bills paid is %s " % (user1Name, str(round(user1total, 2))))
+print (" %s's total amount in bills paid is %s " % (user2Name, str(round(user2total, 2))))
+print("\n")
 
+if user1total > user2total: # if user1 paid more in bills
+    difference = user1total - user2total
+    amountOwed = difference / 2
+    print (" %s owes %s %d " % (user2Name, user1Name, amountOwed))
+if user1total < user2total: # if user2 paid more in bills
+    difference = user2total - user1total
+    amountOwed = difference / 2
+    print (" %s owes %s %d " % (user1Name, user2Name, amountOwed))
+else:
+    amountOwed = 0
+    print (" Each person paid the same amount in bills this month ")
 
-i = 0
-emptyValue = 0.
-while user1Amounts[i] != emptyValue:
-    moneyFormat = str(round(user1Amounts[i], 2))
-    print (" %s : $%s " % (user1Bills[i], moneyFormat))
-    i += 1
+# i = 0
+#emptyValue = 0.
+#while user1Amounts[i] != emptyValue:
+    #moneyFormat = str(round(user1Amounts[i], 2))
+    #print (" %s : $%s " % (user1Bills[i], moneyFormat))
+    #i += 1
