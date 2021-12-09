8b7e0842e53f96272c186047aeb9391c59e4eda7
E-Justin <67754579+E-Justin@users.noreply.github.com>
2021-12-09 07:37:47 -0600

    Update rotateArray.py

--- rotateArray.py
+++ rotateArray.py
@@ -1,19 +1,25 @@
 # task: rotate array k times
-nums = [1,2,3,4,5,6,7]
-k = 3
+nums = [-1,-100,3,99]
+k = 2
 count = 0
 i = 0
+length = len(nums)
 
 print (" Printing elements in array")
 while i < len(nums):
     print (nums[i])
     i += 1
 
-while count < 3:
-    first = nums[0]
-    nums.remove(first)
-    nums.append(first)
-    count += 1
+def rotate(nums, k):
+    count = 0
+    while count < k:
+        last = nums[length - 1] #store value of last element in the array
+        # nums.remove(last) # remove the last element in the array (this one does not work with duplicate values)
+        nums.pop() # removes last item from the array
+        nums.insert(0 , last) # insert the removed value to the front of the array
+        count += 1
+
+rotate(nums, k)
 
 i = 0
 print (" Printing elements in array")
