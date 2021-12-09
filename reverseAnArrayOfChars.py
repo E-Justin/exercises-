4085e1cebe17c6eefc330d81703e9d4aed00aad2
E-Justin <67754579+E-Justin@users.noreply.github.com>
2021-12-09 12:02:51 -0600

    Create reverseAnArrayOfChars.py

---
+++ reverseAnArrayOfChars.py
@@ -0,0 +1,25 @@
+# Write a function that reverses a string. The input string is given as an array of characters s
+
+s = ["J","u","s","t","i","n"]
+length = len(s)
+
+def reverseString(s):
+    
+    i = 0
+    j = length -1
+
+    while i < length / 2:
+        front = s[i]
+        back = s[j]
+        s[i] = back
+        s[j] = front
+        i += 1
+        j -= 1
+    return s
+
+reverseString(s)
+
+i = 0
+while i < length:
+    print(s[i])
+    i += 1
