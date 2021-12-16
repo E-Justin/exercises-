cd7520f8199435015482c2da1c4e513e53360c7d
E-Justin <67754579+E-Justin@users.noreply.github.com>
2021-12-16 14:43:01 -0600

    Create reverseDigitsInInt.py

---
+++ reverseDigitsInInt.py
@@ -0,0 +1,64 @@
+# program to reverse digits in an int
+# dont include zeros
+# if x = 0, return 0
+
+from os import X_OK
+
+
+x = 120
+y = 0
+
+print(x)
+
+
+def reverseNum(x):
+    if x == 0:
+        return 0
+    size = 0
+    y = abs(x)
+    while y != 0:
+        y = y// 10
+        size +=1 # size will hold the number of digits in tht int
+
+    i = -1
+    count = 0
+    if abs(x) != x: # if number is negative
+        x = str(x) # convert to string for easier manipulating
+        while count < size:
+            if x[i] == 0: # dont include 0
+                i -= 1
+            if count == 0:
+                strX = '-' # attach the - sign at the front
+                strX += ''.join(x[i]) # join elements in reverse order
+                count += 1
+                i -= 1
+            else:
+                strX += ''.join(x[i]) # join elements in reverse order
+                count += 1
+                i -= 1
+    else: # if number is positive
+        x = str(x) # convert to string for easier manipulating
+        while count < size:
+            if x[i] == 0:
+                i -= 1 # dont inlcude 0
+            if count == 0:
+                strX = ''.join(x[i]) # join elements in reverse order
+                count += 1
+                i -= 1
+            else:
+                strX += ''.join(x[i]) # join elements in reverse order
+                count += 1
+                i -= 1
+    return int(strX) # return reversed int
+    
+
+    
+strX = reverseNum(x)
+
+print(strX)
+
+
+
+
+
+
