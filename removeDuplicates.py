3d08c9cc497ab32012894f13bc82845aeceb24f2
E-Justin <67754579+E-Justin@users.noreply.github.com>
2021-12-22 08:52:12 -0600

    Update removeDuplicates.py
    faster program now

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -1,40 +1,21 @@
-# program to remove duplicate values.
+ # remove duplicates from array
 
-nums = [1,1]
-#       0 1 2 3 4 5 6 7 8 9
+nums = [0,0,1,1,1,2,2,3,3,4]
+
+
 def removeDuplicates(nums):
-    k = len(nums) # to hold the number of elements in the array = 10
-   
+    k = len(nums) # get length
     i = 0
-    j = i + 1
-    while i < k and j < k:
-        while nums[i] == nums[j]: # if both elements have the same value.
-            nums.pop(i) # removes element from index i
-            k = len(nums)
-            if j == k: # if j has reached past available index
-                break
-        if j < k and nums[i] != nums[j]: # while j is within range and the two values are not the same
-            j += 1
-        if j == k: # if j has reached past available index
-            i += 1 # increment i by one
-            j = i + 1 # set j to the position after i
-        k = len(nums)
 
-    
+    while i < k:
+        look4 = nums[i]
+        k = len(nums) # update length 
+        if nums.count(look4) > 1:
+            nums.remove(look4)
+        else:
+            i += 1
 
-    
-    k = len(nums)
-
-    for i in range(k):
-        print(nums[i])
-
-    print (k)
-    
+    k = len(nums) # update length
     return k
 
-
-
-
-
-    
-    
+removeDuplicates(nums)
