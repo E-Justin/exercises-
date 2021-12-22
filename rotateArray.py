63865b01ba879f63ac6e1d36a30b2dd33daaaad8
E-Justin <67754579+E-Justin@users.noreply.github.com>
2021-12-22 09:13:39 -0600

    Update rotateArray.py

--- rotateArray.py
+++ rotateArray.py
@@ -1,28 +1,20 @@
-# task: rotate array k times
-nums = [-1,-100,3,99]
-k = 2
-count = 0
-i = 0
-length = len(nums)
+# Given an array, rotate the array to the right by k steps, where k is non-negative.
 
-print (" Printing elements in array")
-while i < len(nums):
-    print (nums[i])
-    i += 1
+nums = [1,2,3,4,5,6,7]
+# rotate 1 steps to the right: [7,1,2,3,4,5,6]
+# rotate 2 steps to the right: [6,7,1,2,3,4,5]
+# rotate 3 steps to the right: [5,6,7,1,2,3,4]
+# Output: [5,6,7,1,2,3,4]
+k = 3
 
-def rotate(nums, k):
-    count = 0
-    while count < k:
-        last = nums[length - 1] #store value of last element in the array
-        # nums.remove(last) # remove the last element in the array (this one does not work with duplicate values)
-        nums.pop() # removes last item from the array
-        nums.insert(0 , last) # insert the removed value to the front of the array
-        count += 1
 
-rotate(nums, k)
+def rotateArray(nums, k):
+    i = 0 
 
-i = 0
-print (" Printing elements in array")
-while i < len(nums):
-    print (nums[i])
-    i += 1
+    while i < k:
+        toMove = nums[-1] # stores the value to move
+        nums.pop() # removes last element from array
+        nums.insert(0, toMove) # inserts the value that was removed to the front of the array
+        i += 1
+
+   
