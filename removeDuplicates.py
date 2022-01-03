66ca7c937af0c316b16c547f0eae3b463d6c7a2e
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-01-03 14:52:37 -0600

    Update removeDuplicates.py

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -1,4 +1,4 @@
- # remove duplicates from array
+ # remove duplicates from array and return length (k)
 
 nums = [0,0,1,1,1,2,2,3,3,4]
 
@@ -22,3 +22,23 @@ removeDuplicates(nums)
 
 # Runtime: 2652 ms
 # Memory Usage: 14.5 MB
+
+
+
+
+# or 
+def removeDuplicates2(numArray):
+  k = len(numArray) # get length
+  i = 1
+  j = i -1
+
+  while i < k: # while i is in range
+     if numArray[i] == numArray[j]: # if they are the same
+         numArray.remove(numArray[i]) # remove one
+         k -= 1 # update length after removing an element
+     else:
+         i += 1 # increment i
+         j = i - 1 # increment j
+        
+  return k # return length
+  
