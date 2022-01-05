771c076506833842aeabb35c1a94350eff6d5cd0
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-01-05 06:54:51 -0600

    Update reverseDigitsInInt.py

--- reverseDigitsInInt.py
+++ reverseDigitsInInt.py
@@ -22,3 +22,37 @@ def reverseInt(x) -> int:
     return (x)
 
 print (reverseInt(x))
+
+
+#   or .......
+
+# reverse an integer
+
+x = -123
+
+
+def reverseInt2(int):
+    reversedNumber = 0
+    negative = False
+    if int == 0: # if argument is 0
+        return 0 # return 0
+        
+
+    if int < 0: # if x is negative
+        negative = True
+        int = abs(int) # get absolute value (remove negative sign)
+
+    while int != 0: # while int is not 0
+        remainder = int % 10 # ÷ by 10 and get remainder
+
+        reversedNumber = (reversedNumber * 10) + remainder # math to reverse int one digit at a time
+
+        int = int // 10 # divide by 10 and truncate the decimal (if there is one)
+
+    if negative == True: # if the original argument was negative
+        reversedNumber = -abs(reversedNumber) # put the negative sign back at the front
+
+    print (reversedNumber) # print reversed number
+    return int # return reversed int
+
+reverseInt2(x) # function call
