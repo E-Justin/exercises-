d203598a5d10194d39d61e13f8eacf1485c108b2
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-01-06 06:42:56 -0600

    Update basicLinkedList.py

--- basicLinkedList.py
+++ basicLinkedList.py
@@ -1,28 +1,30 @@
-class LinkedListNode:
-    def __init__ (self, value, nextNode = None):
+class LinkedList:
+    # constructor
+    def __init__(self, value, nextNode = None):
         self.value = value
         self.nextNode = nextNode
 
 
-#initialize nodes
-node1 = LinkedListNode("1")
-node2 = LinkedListNode("2")
-node3 = LinkedListNode("3")
-node4 = LinkedListNode("4")
+node1 = LinkedList("A")
+node2 = LinkedList('B')
+node3 = LinkedList('C')
+node4 = LinkedList('D')
+node5 = LinkedList('E')
 
+# point nodes to the next node in the list
+node1.nextNode = node2
+node2.nextNode = node3
+node3.nextNode = node4
+node4.nextNode = node5
 
-node1.nextNode = node2 # link node 1 to node 2
-node2.nextNode = node3 # link node 2 to node 3
-node3.nextNode = node4 # link node 3 to node 4
-
+# set current node
 currentNode = node1
 
-while True:
-    print(currentNode.value, "->")
-    currentNode = currentNode.nextNode # move to next node
-    if currentNode == None: # if it has reached the end of the list
-        print("None") # it points to nothing
+while True: # while something is there...
+    print (currentNode.value, '->') # print the value of currentNode
+    currentNode = currentNode.nextNode # move currentNode to the next position in the list
+    if currentNode == None: # if it has reached the end of the list...
+        print ("None") # print none (bc it doesnt point to anything)
         break # exit loop
-
 
 
