e20401771db7b9b25f8ebb173039dd3d899814e5
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-01-14 06:59:45 -0600

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,39 +1,37 @@
 class Node:
-    def __init__ (self, data):
+    def __init__(self, data):
         self.right = None
         self.left = None
         self.data = data
-
+    
+    #method to insert into tree
     def insert(self, data):
-        if self.data is None: # ! if there is no data in the root
-            self.data = data # insert the data into the root
-        else: # if there is data in the root
-            if data < self.data: # ! if the data to be added is less than the current node
-                if self.left is None: # ! if there is nothing in the left branch
-                    self.left = Node(data) #insert data in the left branch
-                else: #! if there is data in the left branch
-                    self.left.insert(data) # recursively check the left branches until data can be added
-            elif data > self.data: # ! if data to be inserted is greater than the current node
-                if self.right is None: # ! if there is nothing in the right branch
-                    self.right = Node(data) # insert data in the right branch
-                else: # ! if there is data in the right branch
-                    self.right.insert(data) # recursively check the right branches until data can be added
-                
-        def printTree(self):
-        if self.left: # if there is something in the left branch
-            self.left.printTree()
-        print(self.data) # print root
-        if self.right:# if there is something in the right branch
-            self.right.printTree() 
+        if self.data is None:
+            self.data = data
+        else: # if there is data in the tree
+            if data < self.data: # if the data to be inserted is less than the current node
+                if self.left is None: # if there is nothing in the left branch
+                    self.left = Node(data)
+                else: # if there is data in the left branch
+                    self.left.insert(data)
+            elif data > self.data: # if the data to be inserted is greater than the current node
+                if self.right is None: # if there is nothing in the right branch
+                    self.right = Node(data)
+                else: # if there is data in the right branch
+                    self.right.insert(data)
+                    
+    # method to print tree
+    def printTree(self):
+        if self.left: # if there is a left branch
+            self.left.printTree() # prints left branch
+        print(self.data) # prints root
+        if self.right: # if there is a right branch
+            self.right.printTree() # prints right branch
 
-root = Node('g')
-root.insert('c')
-root.insert('b')
-root.insert('a')
-root.insert('e')
-root.insert('d')
-root.insert('f')
-root.insert('i')
-root.insert('h')
-root.insert('j')
-root.insert('k')
+myTree = Node(7) # create root
+
+myTree.insert(15) # insert
+myTree.insert(5)
+myTree.insert(82)
+myTree.insert(2.4)
+myTree.printTree() # print
