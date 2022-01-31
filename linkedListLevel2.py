1aea2da4593e314e1cad9a0440594d7841e9b013
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-01-31 06:00:02 -0600

    Update linkedListLevel2.py
    added a new method to findVal() in a linked list

--- linkedListLevel2.py
+++ linkedListLevel2.py
@@ -1,43 +1,82 @@
 class Node:
     def __init__(self, data = None):
         self.data = data
-        self.next = None # set next to None (it does not exist yet)
-
-class LinkedList:
-    # constructor
-    def __init__(self):
-        self.head = Node() # create head Node
-        
-    # method to append a node to the list
-    def append(self, data):
-        newNode = Node(data) # create a new node with the given value
-        currentNode = self.head # start at the front of the list
-        while currentNode.next is not None: # while the next node in line has a value
-            currentNode = currentNode.next # move to the next node
-        currentNode.next = newNode # place new node at the end of the list
-        
-    # method to get length of list
-    def length(self):
-        currentNode = self.head
-        total = 0
-        while currentNode.next is not None:
-            total += 1
-            currentNode = currentNode.next
-        return total
+        self.next = None
     
-    # method to display list
-    def display(self):
-        listArr = [] # create empty array to hold each of the items in the list
-        currentNode = self.head # start at the front of the list
-        while currentNode.next is not None: # while the next node has something in it
-            currentNode = currentNode.next # move node over one
-            listArr.append(currentNode.data) # append array with each item in the list
-        print(listArr) # print list
-            
+class LinkedList:
+    def __init__(self):
+        self.head = Node()
+    
+    def append(self, data):
+        current = self.head
+        newNode = Node(data)
+        while current.next is not None:
+            current = current.next
+        current.next = newNode
+    
+    def length(self):
+        current = self.head
+        total = 0
+        while current.next is not None:
+            current = current.next
+            total += 1
+        return total
 
-List = LinkedList() # instantiate linked list
-List.display() # display (nothing here yet)
-List.append(12) # append 12
-List.append(22) # append 22
-List.display() # display list (12, 22)
-print(List.length()) # print length
+    def display(self):
+        current = self.head
+        listArr = []
+        while current.next is not None:
+            current = current.next
+            listArr.append(current.data)
+        print(listArr)
+    
+    def findVal(self, data):
+        current = self.head
+        while current.next is not None:
+            current = current.next
+            if current.data == data:
+                return (str(data) + " was found ")
+        return (str(data) + " was NOT found ")
+
+
+    def deleteNode(self, data):
+        temp = self.head # store head node
+
+        if temp is not None: # if head node is the value to be deleted
+            if temp.data == data:
+                self.head = temp.next
+                temp = None
+                return
+
+        # search for the value to be deleted and keep track of the 
+        # previous node as we need to change prev.next
+        while temp is not None:
+            if temp.data == data:
+                break
+            prev = temp
+            temp = temp.next
+
+        # if the value to be deleted does not exist in the list
+        if temp is None:
+            return 
+        
+        #unlink the node from linked list
+        prev.next = temp.next
+        temp = None
+
+
+List = LinkedList()
+List.append(17)
+List.append(5)
+List.append(16)
+List.append(7)
+print(List.findVal(17))
+print(List.findVal(5))
+
+List.display()
+print(List.length())
+
+List.deleteNode(5)
+List.display()
+print(List.length())
+print(List.findVal(5))
