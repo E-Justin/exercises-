243eab113534489a665f1948cae07a5400bb5b58
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-03-09 14:25:21 -0600

    Update linkedListLevel2.py

--- linkedListLevel2.py
+++ linkedListLevel2.py
@@ -8,27 +8,27 @@ class LinkedList:
         self.head = Node()
     
     def append(self, data):
-        current = self.head 
-        newNode = Node(data)
-        while current.next is not None:
-            current = current.next
-        current.next = newNode
+        current = self.head # start at front
+        newNode = Node(data) # create newNode to add the data to the list
+        while current.next is not None: # while the end of the list has not been reached
+            current = current.next # move to the next position
+        current.next = newNode # when the end of the list has been reached, insert the newNode
     
     def length(self):
-        current = self.head
-        total = 0
-        while current.next is not None:
-            current = current.next
-            total += 1
+        current = self.head # start at the front
+        total = 0 # set total to 0
+        while current.next is not None: # whiel the end of the list has not been reached
+            current = current.next # move to the next position
+            total += 1 # add one to total
         return total
 
     def display(self):
-        current = self.head
-        listArr = []
-        while current.next is not None:
-            current = current.next
-            listArr.append(current.data)
-        print(listArr)
+        current = self.head # start at front
+        listArr = [] # create an emtpy list to append data values to later
+        while current.next is not None: # while the end of the list has not been reached
+            current = current.next # move to the next position
+            listArr.append(current.data) # append each data value, one at a time to the list
+        print(listArr) # print the list with all values from linked list
     
     def findVal(self, data):
         current = self.head # start at front of list
@@ -43,22 +43,22 @@ class LinkedList:
         current = self.head # start at head
 
         # if the head node is the one to be deleted
-        if current is not None:
-            if current.data == data:
-                self.head = current.next
-                current = None
+        if current is not None: # if there is something there
+            if current.data == data: # if value to be deleted has been found at the head node
+                self.head = current.next # move head of the list over one
+                current = None # delete node
                 return None
 
         # if the node to be deleted is not the head node
         prev = None
-        while current is not None:
-            if current.data == data:
+        while current is not None: # while the end of the list has not been reached
+            if current.data == data: # if the value to be deleted has been found
                 break
-            prev = current
-            current = current.next
+            prev = current #  prev holds current's previous place
+            current = current.next # move to next node
 
         # if there is nothing in the list/ if the end of the list has been reached without finding the value to delete
-        if current is None:
+        if current is None: 
             return None
 
         prev.next = current.next # skip over the node to be deleted
