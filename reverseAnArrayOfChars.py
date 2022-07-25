394523f3c3e99488e1d6d2a631bddebbd8177a60
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-07-25 07:54:51 -0700

    Update reverseAnArrayOfChars.py

--- reverseAnArrayOfChars.py
+++ reverseAnArrayOfChars.py
@@ -45,5 +45,27 @@ def reverseTheArray(arr):
     
     return arr
 
+# or ........
+
+def alpha_sort(a_list: list) -> list:
+    length = len(a_list) - 1  # length / last available index
+
+    for i in range(length):  # iterate through list one time for each item
+        list_changed = False  # flag to let program know that changes were or were not made
+        for j in range(length):  # iterate through previous for loop (length * length) times
+            current = a_list[j]  # get item
+            after = a_list[j + 1]  # get item next to current
+
+            if current > after:  # if items are in descending order
+                a_list[j], a_list[j + 1] = a_list[j+1], a_list[j]  # switch them
+                list_changed = True  # flag to let program know that changes were made
+        if list_changed is False:  # if no changes were made
+            break  # exit loop
+
+    return a_list  # return sorted list
+
+
+
+
 # runtime: 203 ms
 # memory usage: 21.2MB
